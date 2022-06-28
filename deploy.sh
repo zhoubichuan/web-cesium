aa3aec7eb93c0b7b8c6218a47cdbcff848655262
zhoubichuan <zhoubichuan@icloud.com>
2022-06-28 23:45:11 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 # 1.发布到master分支
-# git add -A
-# git commit -m 'master'
-# git pull gitee master
-# git push gitee master
+git add -A
+git commit -m 'master'
+git pull gitee master
+git push gitee master
 
 # 2.打包
 npm run src:build
 
-cd dist
+cd web-cesium
 # 3.发布到gh-pages分支
 git init
 git checkout --orphan gh-pages
@@ -24,20 +24,3 @@ git add -A
 git commit -m 'master'
 git pull github master
 git push github master
-
-# 2.打包
-npm run src:build
-
-# 3.进入打包目录
-cd dist
-
-# 4.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add github https://github.com/zhoubichuan/web-cesium.git
-git push -f github gh-pages
-
-# 5.返回初始目录
-cd -
